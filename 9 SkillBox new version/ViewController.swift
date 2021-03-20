398aec49a4c3624b9b8b32584e5dfc0413b24c98
Kirill12a <kirilldrozd12@gmail.com>
2021-03-20 20:49:43 +0300

    Initial Commit

---
+++ 9 SkillBox new version/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  9 SkillBox new version
+//
+//  Created by Kirill Drozdov on 20.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
